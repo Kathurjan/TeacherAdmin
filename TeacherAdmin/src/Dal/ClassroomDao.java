faef5f82ff119e91582768113f05c22df2622238
Kathurjan <kath097c@easv365.dk>
2022-02-22 10:22:28 +0100

    Update ClassroomDao.java

--- TeacherAdmin/src/Dal/ClassroomDao.java
+++ TeacherAdmin/src/Dal/ClassroomDao.java
@@ -26,4 +26,21 @@ public class ClassroomDao {
 
 
     }
+
+    public ObservableList<Student> getStudent1(){
+
+        ObservableList<Student> students1 = FXCollections.observableArrayList();
+
+        students1.add(new Student("Bob",1,62/100));
+        students1.add(new Student("Bob1",2,51/100));
+        students1.add(new Student("Bob3",3,53/100));
+        students1.add(new Student("Bob4",5,54/100));
+        students1.add(new Student("Bob5",6,54/100));
+        students1.add(new Student("Bob6",7,54/100));
+        students1.add(new Student("Bob7",8,54/100));
+        return students1;
+
+
+
+    }
 }
